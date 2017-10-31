f144fe2485688be4a505875d302ddf3c40911071
Igor Altieres <igoraltieres@gmail.com>
2017-10-31 12:21:24 -0200

    Respawn parte 1 (16 min)

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour { 
+
+ public static GM instance = null;
+
+public float yMinLive = -10f;
+public Transform spawnPoint;
+public GameObject playerPrefab;
+
+PlayerCtrl player;
+
+ 	void Awake(){
+		 if(instance == null) {
+			 instance = this;
+		 }
+		 else if (instance != this ) {
+			 Destroy(gameObject);
+		 }
+		 DontDestroyOnLoad(gameObject);
+	 }
+
+ 	void Start () {
+		 if (player == null) {
+			 RespawnPlayer();
+		 }
+		
+	}
+	
+	void Update () {
+		
+	}
+	public void RespawnPlayer() {
+	 Instantiate (playerPrefab, spawnPoint.position, spawnPoint.rotation);
+	}
+}
