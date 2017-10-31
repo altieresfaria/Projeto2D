b9c7758adb8318924b1e87c9bbfbed24dadcb61e
Igor Altieres <igoraltieres@gmail.com>
2017-10-31 10:57:51 -0200

    Single Jump

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -25,6 +25,16 @@ public class PlayerCtrl : MonoBehaviour {
 	Animator anim;
 	bool isJumping = false;
 
+	public Transform feet;
+
+	public float feetWidht = 0.5f;
+
+	public float feetHeight = 0.1f;
+
+	public bool isGrounded;
+	public LayerMask whatIsGround;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -32,9 +42,15 @@ public class PlayerCtrl : MonoBehaviour {
 	sr = GetComponent<SpriteRenderer>();
 	anim = GetComponent<Animator>();	
 	}
+
+	void OnDrawGizmos() {
+		Gizmos.DrawWireCube(feet.position, new Vector3(feetWidht, feetHeight, 0f));
+	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		isGrounded = Physics2D.OverlapBox(new Vector2 (feet.position.x, feet.position.y), new Vector2(feetWidht,feetHeight), 360.0f, whatIsGround);
 
 		float horizantalInput = Input.GetAxisRaw("Horizontal"); //-1: esquerda, 1: direita 
 		float horizontalPlayerSpeed = horizontalSpeed * horizantalInput;
@@ -71,11 +87,12 @@ public class PlayerCtrl : MonoBehaviour {
 		}
 	}
 		void Jump() {
+			if(isGrounded) {
 		isJumping = true;
 		rb.AddForce (new Vector2(0f, JumpSpeed));
 		anim.SetInteger("State", 1);
 	}
-	
+		}
 
 	void StopMovingHorizontal() {
 	rb.velocity = new Vector2(0f, rb.velocity.y);	
