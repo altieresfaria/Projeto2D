bbd8189cee07e6238e42562c4a067ef1efc89d92
Igor Altieres <igoraltieres@gmail.com>
2017-11-07 11:10:48 -0200

    moeda em PlayerCtrl

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -130,5 +130,10 @@ public class PlayerCtrl : MonoBehaviour {
 
 		}
 	}
-
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag("Coin")) {
+			Destroy(other.gameObject);
+		}
+	}
 }
